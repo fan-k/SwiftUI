7ccddfcc3f44a626945136548457410d863e740d
酒酿小汤圆~ <15936219505@163.com>
2023-11-27 15:49:19 +0800

    init

---
+++ SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by 樊康鹏 on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
